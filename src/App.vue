02519bd472a291893c4dde23165e03d801467ccd
Mostafa Said <94674993+MooseSaeed@users.noreply.github.com>
2022-03-14 18:09:08 +0200

    creating Navbar component

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,22 @@
 <template>
+  <div class="fixed bottom-0 left-0 right-0">
+    <Navbar />
+  </div>
   <router-view />
 </template>
 
+<script>
+import Navbar from "./components/Navbar.vue";
+export default {
+  components: {
+    Navbar,
+  },
+};
+</script>
+
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  background: radial-gradient(ellipse at bottom, #1b2735 0%, #090a0f 100%);
+  height: 100vh;
 }
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,96 @@
+<template>
+  <div class="container">
+    <div class="ui" id="js-ui">
+      <nav class="scene_nav">
+        <ol class="scene_nav_list">
+          <li
+            class="scene_nav_item"
+            v-for="(route, index) in maxScene"
+            :key="route"
+          >
+            <button
+              class="scene_nav_button"
+              @click="scene = index"
+              :class="{ 'o-active': scene === index }"
+            >
+              <p>{{ route.title }}</p>
+            </button>
+          </li>
+        </ol>
+      </nav>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      scene: 0,
+      maxScene: [
+        { index: 1, title: "Live Transcription" },
+        { index: 2, title: "hey" },
+      ],
+    };
+  },
+
+  mounted() {
+    setTimeout(() => {
+      this.scene = this.maxScene - 2;
+    }, 1000);
+  },
+};
+</script>
+
+<style>
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
+}
+
+.scene_nav {
+  position: relative;
+}
+.scene_nav_list {
+  display: flex;
+  margin: 0;
+  padding: 0;
+  transform: scaleY(0.6);
+}
+.scene_nav_item {
+  display: block;
+}
+.scene_nav_item:nth-child(even) {
+  transform: scaleY(-1);
+}
+
+.scene_nav_item:nth-child(even) p {
+  transform: scaleY(-1);
+}
+
+.scene_nav_item:not(:first-child) {
+  margin-left: -8px;
+}
+.scene_nav_button {
+  display: block;
+  width: 10rem;
+  height: 50px;
+  margin: 0;
+  padding: 0;
+  border: none;
+  background: #0a7896;
+  outline: none;
+  cursor: pointer;
+  transition: 500ms;
+  -webkit-clip-path: polygon(25px 0, calc(100% - 25px) 0, 100% 100%, 0 100%);
+  clip-path: polygon(25px 0, calc(100% - 25px) 0, 100% 100%, 0 100%);
+}
+.scene_nav_button.o-active {
+  width: 12rem;
+  background: #64fafa;
+}
+.scene_nav_button:hover {
+  background: #64fafa;
+}
+</style>
