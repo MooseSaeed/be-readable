77fb63982ad91b6c6cad1c43adbddf16af025989
Mostafa Said <94674993+MooseSaeed@users.noreply.github.com>
2022-03-18 23:36:43 +0200

    finalizing

--- src/components/Recorder.vue
+++ src/components/Recorder.vue
@@ -3,7 +3,7 @@
 </template>
 
 <script>
-import TapirWidget from "./audiorecorder/TapirWidget.vue";
+import TapirWidget from "vue-audio-tapir/src/components/TapirWidget.vue";
 import "vue-audio-tapir/dist/vue-audio-tapir.css";
 
 export default {
